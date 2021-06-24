bf71077735438e39c9f84ea7aa40db033934b959
rafael-503 <73114569+rafael-503@users.noreply.github.com>
2021-06-24 11:32:00 -0300

    aula 2

---
+++ lib/shared/widgets/social_login/social_login_button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  final VoidCallback ontap;
+  const SocialLoginButton({Key? key, required this.ontap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return InkWell(
+      onTap: ontap,
+      child: Container(
+        height: size.height * 0.074,
+        width: size.width * 0.8,
+        decoration: BoxDecoration(
+            color: AppColors.shape,
+            borderRadius: BorderRadius.circular(5),
+            border: Border.fromBorderSide(BorderSide(color: AppColors.stroke))),
+        child: Row(
+          children: [
+            Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(AppImages.google),
+                    SizedBox(width: size.width * 0.0426),
+                    Container(
+                      height: 56,
+                      width: 1,
+                      color: AppColors.stroke,
+                    )
+                  ],
+                )),
+            Expanded(
+              flex: 4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Entrar com Google',
+                    style: TextStyles.buttonGray,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
