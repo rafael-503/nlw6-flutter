7d14b40dd1a86536c1da25243967180b4abca590
rafael-503 <73114569+rafael-503@users.noreply.github.com>
2021-07-02 03:25:23 -0300

    aula-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app_widget.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(AppFirebase());
 }
 
